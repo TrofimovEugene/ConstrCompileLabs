84e37407f7d4485011c186f4b5c6090d853daaab
Trofimov Eugene <evgentrofi@gmail.com>
2021-03-09 20:53:37 +0800

    first commit

---
+++ Lab2_1/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Lab2_1
+{
+    public class Program
+    {
+        public static int State;
+
+        public static void Main(string[] args)
+        {
+            //var stringInput = Console.ReadLine();
+            /*var table = new[,] { 
+                {null, "A", "B", "C"},
+                {"0", "1", "0", "0"},
+                {"1", "1", "2", "0"},
+                {"2", "3", "0", "0"},
+                {"3", "1", "4", "0"},
+                {"4", "5", "0", "0"},
+                {"5", "1", "5", "6"}
+            };*/
+
+            var s = "ABABAC";
+            var reg_ex = new Regex(@"[A-Z]");
+            var matches_a = reg_ex.Matches(s);
+            State = 0;
+            foreach (Match match in matches_a)
+            {
+                switch (State)
+                {
+                    case 0:
+                        switch (match.Value)
+                        {
+                            case "A":
+                                State = 1;
+                                break;
+                            case "B":
+                                continue;
+                            case "C":
+                                continue;
+                            default:
+                                Console.WriteLine("Цепочка не распознана");
+                                State = -1;
+                                break;
+                        }
+                        continue;
+                    case 1:
+                        switch (match.Value)
+                        {
+                            case "A":
+                                continue;
+                            case "B":
+                                State = 2;
+                                break;
+                            case "C":
+                                State = 0;
+                                break;
+                            default:
+                                Console.WriteLine("Цепочка не распознана");
+                                State = -1;
+                                break;
+                        }
+                        continue;
+                    case 2:
+                        switch (match.Value)
+                        {
+                            case "A":
+                                State = 3;
+                                break;
+                            case "B":
+                                State = 0;
+                                break;
+                            case "C":
+                                State = 0;
+                                break;
+                            default:
+                                Console.WriteLine("Цепочка не распознана");
+                                State = -1;
+                                break;
+                        }
+                        continue;
+                    case 3:
+                        switch (match.Value)
+                        {
+                            case "A":
+                                State = 1;
+                                break;
+                            case "B":
+                                State = 4;
+                                break;
+                            case "C":
+                                State = 0;
+                                break;
+                            default:
+                                Console.WriteLine("Цепочка не распознана");
+                                State = -1;
+                                break;
+                        }
+                        continue;
+                    case 4:
+                        switch (match.Value)
+                        {
+                            case "A":
+                                State = 5;
+                                break;
+                            case "B":
+                                State = 0;
+                                break;
+                            case "C":
+                                State = 0;
+                                break;
+                            default:
+                                Console.WriteLine("Цепочка не распознана");
+                                State = -1;
+                                break;
+                        }
+                        continue;
+                    case 5:
+                        switch (match.Value)
+                        {
+                            case "A":
+                                State = 1;
+                                break;
+                            case "B":
+                                State = 4;
+                                break;
+                            case "C":
+                                State = 6;
+                                break;
+                            default:
+                                Console.WriteLine("Цепочка не распознана");
+                                State = -1;
+                                break;
+                        }
+                        continue;
+                }
+            }
+            
+            Console.WriteLine(State);
+        }
+    }
+}
---
+++ Lab2_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2_2
+{
+    public class Program
+    {
+        public static string State;
+        public static void Main(string[] args)
+        {
+            var s = "0000011111";
+            var stack = new Stack<char>();
+            State = "N";
+            try
+            {
+                foreach (var ch in s)
+                {
+                    switch (ch)
+                    {
+                        case '0':
+                            stack.Push(ch);
+                            State = "0";
+                            break;
+                        case '1':
+                            stack.Pop();
+                            State = "1";
+                            break;
+                    }
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                // ignore
+            }
+            
+            if (stack.TryPop(out  _))
+                Console.WriteLine("Цепочка не распознана. Нулей больше чем единиц");
+        }
+    }
+}
