5983cd64952b062d8b08c59027cc70680eeef5fd
Trofimov Eugene <evgentrofi@gmail.com>
2021-04-21 23:49:27 +0800

    сделал пятую лабораторную

---
+++ Lab5/ActionType.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab5
+{
+    public enum ActionType
+    {
+        Shift = 0,
+        Reduce = 1,
+        Accept = 2,
+        Error = 3
+    }
+}
--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LR_parser
+namespace Lab5
 {
     public class Program
     {
-        private const string Str = "a*a-a#";
+        private const string Str = "(a+a)+a#";
         private static Stack<int> _stateStack;
         private static int _index = 0;
         public static void Main(string[] args)
@@ -51,22 +51,17 @@ namespace LR_parser
                     switch (_stateStack.Peek())
                     {
                         case 0:
-                            _stateStack.Push(5);
+                            _stateStack.Push(3);
                             stack.Push(a);
                             _index++;
                             return ActionType.Shift;
                         case 4:
-                            _stateStack.Push(5);
+                            _stateStack.Push(3);
                             stack.Push(a);
                             _index++;
                             return ActionType.Shift;
-                        case 6:
-                            _stateStack.Push(5);
-                            stack.Push(a);
-                            _index++;
-                            return ActionType.Shift;
-                        case 7:
-                            _stateStack.Push(5);
+                        case 5:
+                            _stateStack.Push(3);
                             stack.Push(a);
                             _index++;
                             return ActionType.Shift;
@@ -77,13 +72,7 @@ namespace LR_parser
                             return ActionType.Error;
                     }
                 case '+':
-                    return ActionForPlusOrMinus(stack, a);
-                case '-':
-                    return ActionForPlusOrMinus(stack, a);
-                case '*':
-                    return ActionForMultipleOrDivide(stack, a);
-                case '/':
-                    return ActionForMultipleOrDivide(stack, a);
+                    return ActionForPlus(stack, a);
                 case '(':
                     switch (_stateStack.Peek())
                     {
@@ -97,12 +86,7 @@ namespace LR_parser
                             stack.Push(a);
                             _index++;
                             return ActionType.Shift;
-                        case 6:
-                            _stateStack.Push(4);
-                            stack.Push(a);
-                            _index++;
-                            return ActionType.Shift;
-                        case 7:
+                        case 5:
                             _stateStack.Push(4);
                             stack.Push(a);
                             _index++;
@@ -119,28 +103,20 @@ namespace LR_parser
                             GoTo(stack);
                             return ActionType.Reduce;
                         case 3:
-                            R4(stack);
-                            GoTo(stack);
-                            return ActionType.Reduce;
-                        case 5:
-                            R6(stack);
-                            GoTo(stack);
-                            return ActionType.Reduce;
-                        case 8:
-                            _stateStack.Push(11);
-                            stack.Push(a);
-                            _index++;
-                            return ActionType.Shift;
-                        case 9:
-                            R1(stack);
-                            GoTo(stack);
-                            return ActionType.Reduce;
-                        case 10:
                             R3(stack);
                             GoTo(stack);
                             return ActionType.Reduce;
-                        case 11:
-                            R5(stack);
+                        case 6:
+                            _stateStack.Push(8);
+                            stack.Push(a);
+                            _index++;
+                            return ActionType.Shift;
+                        case 7:
+                            R1(stack);
+                            GoTo(stack);
+                            return ActionType.Reduce;
+                        case 8:
+                            R4(stack);
                             GoTo(stack);
                             return ActionType.Reduce;
                         default:
@@ -157,23 +133,15 @@ namespace LR_parser
                             GoTo(stack);
                             return ActionType.Reduce;
                         case 3:
-                            R4(stack);
-                            GoTo(stack);
-                            return ActionType.Reduce;
-                        case 5:
-                            R6(stack);
-                            GoTo(stack);
-                            return ActionType.Reduce;
-                        case 9:
-                            R1(stack);
-                            GoTo(stack);
-                            return ActionType.Reduce;
-                        case 10:
                             R3(stack);
                             GoTo(stack);
                             return ActionType.Reduce;
-                        case 11:
-                            R5(stack);
+                        case 7:
+                            R1(stack);
+                            GoTo(stack);
+                            return ActionType.Reduce;
+                        case 8:
+                            R4(stack);
                             GoTo(stack);
                             return ActionType.Reduce;
                         default:
@@ -197,7 +165,7 @@ namespace LR_parser
                             _stateStack.Push(1);
                             break;
                         case 4:
-                            _stateStack.Push(8);
+                            _stateStack.Push(6);
                             break;
                         default:
                             _stateStack.Push(20);
@@ -214,29 +182,8 @@ namespace LR_parser
                         case 4:
                             _stateStack.Push(2);
                             break;
-                        case 6:
-                            _stateStack.Push(9);
-                            break;
-                        default:
-                            _stateStack.Push(20);
-                            Console.WriteLine("errr");
-                            break;
-                    }
-                    break;
-                case 'F':
-                    switch (_stateStack.Peek())
-                    {
-                        case 0:
-                            _stateStack.Push(3);
-                            break;
-                        case 4:
-                            _stateStack.Push(3);
-                            break;
-                        case 6:
-                            _stateStack.Push(3);
-                            break;
-                        case 7:
-                            _stateStack.Push(10);
+                        case 5:
+                            _stateStack.Push(7);
                             break;
                         default:
                             _stateStack.Push(20);
@@ -274,6 +221,15 @@ namespace LR_parser
 
         private static void R3(Stack<char> stack)
         {
+            _stateStack.Pop();
+            
+            var symbol = stack.Pop();
+            stack.Push('T');
+            Console.WriteLine($"T -> {symbol}");
+        }
+        
+        private static void R4(Stack<char> stack)
+        {
             var reduce = new StringBuilder("");
             for (var i = 1; i < 4; i++)
             {
@@ -285,45 +241,12 @@ namespace LR_parser
             Console.WriteLine($"T -> {reduce}");
         }
 
-        private static void R4(Stack<char> stack)
-        {
-            _stateStack.Pop();
-            
-            var symbol = stack.Pop();
-            stack.Push('T');
-            Console.WriteLine($"T -> {symbol}");
-        }
-
-        private static void R5(Stack<char> stack)
-        {
-            
-            
-            var reduce = new StringBuilder("");
-            for (var i = 1; i < 4; i++)
-            {
-                _stateStack.Pop();
-                reduce.Append(stack.Pop());
-            }
-            stack.Push('F');
-            
-            Console.WriteLine($"F -> {reduce}");
-        }
-
-        private static void R6(Stack<char> stack)
-        {
-            _stateStack.Pop();
-            
-            var symbol = stack.Pop();
-            stack.Push('F');
-            Console.WriteLine($"F -> {symbol}");
-        }
-
-        private static ActionType ActionForPlusOrMinus(Stack<char> stack, char a)
+        private static ActionType ActionForPlus(Stack<char> stack, char a)
         {
             switch (_stateStack.Peek())
             {
                 case 1:
-                    _stateStack.Push(6);
+                    _stateStack.Push(5);
                     stack.Push(a);
                     _index++;
                     return ActionType.Shift;
@@ -332,65 +255,20 @@ namespace LR_parser
                     GoTo(stack);
                     return ActionType.Reduce;
                 case 3:
-                    R4(stack);
+                    R3(stack);
                     GoTo(stack);
                     return ActionType.Reduce;
-                case 5:
-                    R6(stack);
-                    GoTo(stack);
-                    return ActionType.Reduce;
-                case 8:
-                    R6(stack);
-                    GoTo(stack);
+                case 6:
+                    _stateStack.Push(5);
                     stack.Push(a);
                     _index++;
                     return ActionType.Shift;
-                case 9:
+                case 7:
                     R1(stack);
                     GoTo(stack);
                     return ActionType.Reduce;
-                case 10:
-                    R3(stack);
-                    GoTo(stack);
-                    return ActionType.Reduce;
-                case 11:
-                    R5(stack);
-                    GoTo(stack);
-                    return ActionType.Reduce;
-                default:
-                    _stateStack.Push(20);
-                    return ActionType.Error;
-            }
-        }
-
-        private static ActionType ActionForMultipleOrDivide(Stack<char> stack, char a)
-        {
-            switch (_stateStack.Peek())
-            {
-                case 2:
-                    _stateStack.Push(7);
-                    stack.Push(a);
-                    _index++;
-                    return ActionType.Shift;
-                case 3:
+                case 8:
                     R4(stack);
-                    GoTo(stack);
-                    return ActionType.Reduce;
-                case 5:
-                    R6(stack);
-                    GoTo(stack);
-                    return ActionType.Reduce;
-                case 9:
-                    _stateStack.Push(7);
-                    stack.Push(a);
-                    _index++;
-                    return ActionType.Shift;
-                case 10:
-                    R3(stack);
-                    GoTo(stack);
-                    return ActionType.Reduce;
-                case 11:
-                    R5(stack);
                     GoTo(stack);
                     return ActionType.Reduce;
                 default:
